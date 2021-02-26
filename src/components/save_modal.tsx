9086508bf97699449e7b21915dcb059f276e8acd
mzw86020424 <mzw86020424@gmail.com>
2021-02-26 10:27:14 +0900

    モーダルで

--- src/components/save_modal.tsx
+++ src/components/save_modal.tsx
@@ -47,9 +47,20 @@ export const SaveModal: React.FC<Props> = props => {
         <p>テキストの内容を保存します。</p>
         <p>保存内容のタイトルを入力して「保存」ボタンを押してください。</p>
         <p>
-
-          
+          <TitleInput
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
         </p>
+        <Control>
+          <Button onClick={onCancel} cancel>
+            キャンセル
+          </Button>
+          <Button onClick={() => onSave(title)}>
+            保存
+          </Button>
+        </Control>
       </Modal>
     </Wrapper>
   )
