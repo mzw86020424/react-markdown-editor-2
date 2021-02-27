8b4ee649f5deb4158bf6bde2e5d2a8387960d70c
mzw86020424 <mzw86020424@gmail.com>
2021-02-27 12:53:38 +0900

    履歴ページの実装

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -48,13 +48,14 @@ const Preview = styled.div`
   top: 0;
   width: 50vw;
 `
-const StorageKey = 'pages/editor:text'
+interface Props {
+  text: string
+  setText: (text: string) => void
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage('', StorageKey)
-  
+export const Editor: React.FC<Props> = (props) => {
+  const { text, setText } = props
   const [showModal, setShowModal] = useState(false)
-  
 
   return (
     <>
